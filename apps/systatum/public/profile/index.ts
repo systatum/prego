5ac4a04c2cec0950735c85b12af86473d24362e5
Alimnfll <alim@systatum.com>
2025-06-20 19:32:07 +0700

    Add profile image collaborator.

---
+++ apps/systatum/public/profile/index.ts
@@ -0,0 +1,15 @@
+import ProfileAdam from "./profile-adam.png";
+import ProfileAlim from "./profile-alim.jpeg";
+import ProfileGiberdi from "./profile-giberdi.png";
+import ProfileKade from "./profile-kade.png";
+import ProfileKent from "./profile-kent.png";
+import ProfileSalwa from "./profile-salwa.png";
+
+export {
+  ProfileAdam,
+  ProfileAlim,
+  ProfileGiberdi,
+  ProfileKade,
+  ProfileKent,
+  ProfileSalwa,
+};
