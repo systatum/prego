f2109a36c74542f673f8a23979dbc59e78bbb488
Adam Hakarsa <adamnoto@hey.com>
2025-06-23 15:03:04 +0700

    Use BUILD_ENV instead of NODE_ENV
    This is avoid this error:
    
    2:44:29 PM: WARNING: The environment variable 'NODE_ENV' is set to 'production'. Any 'devDependencies' in package.json will not be installed
    
    Which prevents proper building.

--- apps/systatum/next.config.ts
+++ apps/systatum/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: process.env.NODE_ENV === "production" ? "export" : "standalone",
+  output: process.env.BUILD_ENV === "production" ? "export" : "standalone",
   /* config options here */
 };
 
