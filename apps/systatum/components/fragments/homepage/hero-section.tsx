83ad7a0c8d3079faf774500cc554fd84af8663b6
Alimnfll <alim@systatum.com>
2025-06-23 12:32:08 +0700

    Update after scroll if condition not have separated.

--- apps/systatum/components/fragments/homepage/hero-section.tsx
+++ apps/systatum/components/fragments/homepage/hero-section.tsx
@@ -9,7 +9,16 @@ import { cn } from "@/lib/utils";
 export default function Hero() {
   const [displayedText, setDisplayedText] = useState("");
   const [showOtherElements, setShowOtherElements] = useState(false);
-  const [isDarkBackground, setIsDarkBackground] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 130);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   const fullText = "System behind you.";
 
@@ -30,43 +39,50 @@ export default function Hero() {
     return () => clearInterval(typingInterval);
   }, []);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const section = document.getElementById("dark-section");
-      const textY = 16;
-
-      if (section) {
-        const rect = section.getBoundingClientRect();
-
-        if (rect.top <= textY && rect.bottom >= textY) {
-          setIsDarkBackground(true);
-        } else {
-          setIsDarkBackground(false);
-        }
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    handleScroll();
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
-    <div className="flex flex-col min-h-screen sm:min-h-[780px] lg:min-h-screen justify-between sm:pt-[120px] lg:pt-[150px] overflow-hidden border-b gap-20 sm:gap-36 pb-[200px] sm:pb-[250px] lg:pb-[80px] lg:gap-0 relative">
+    <div className="flex flex-col min-h-screen sm:min-h-[780px] lg:min-h-screen justify-start sm:pt-[120px] lg:pt-[150px] overflow-hidden gap-20 sm:gap-20 lg:gap-24 relative">
       {showOtherElements && (
-        <motion.div
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
+        <div
+          id="header"
           className={cn(
-            "fixed text-xl montHeavy left-4 top-4 z-20",
-            isDarkBackground ? "text-white title-shadow" : "title-shadow-white"
+            `fixed top-0 left-0 w-full z-50 px-14 sm:px-[50px] md:px-6 lg:px-[50px] py-4`,
+            scrolled
+              ? "bg-white backdrop-blur-md shadow-md md:bg-transparent md:backdrop-blur-none md:shadow-none"
+              : "bg-transparent"
           )}
         >
-          Systatum
-        </motion.div>
+          <div
+            className={cn(
+              "text-xl w-[20px] hidden absolute",
+              scrolled && "flex md:hidden left-6 top-[19px]"
+            )}
+          >
+            <Image alt="Systatum Logo" src={SystatumLogo} width={300} />
+          </div>
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            className="text-xl left-14 md:text-[36px] lg:text-[40px] montHeavy flex flex-row items-center gap-2"
+          >
+            Systatum
+          </motion.div>
+        </div>
       )}
+
+      <AnimatePresence>
+        {showOtherElements && (
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            aria-label="logo-systatum-medium-to-up"
+            className="w-[120px] md:w-[160px] lg:w-[210px] md:left-8 lg:left-12 top-[240px] lg:top-[280px] md:fixed md:flex hidden"
+          >
+            <Image alt="Systatum Logo" src={SystatumLogo} width={300} />
+          </motion.div>
+        )}
+      </AnimatePresence>
 
       {showOtherElements &&
         Array.from({ length: 3 }).map((_, i) => (
@@ -85,14 +101,15 @@ export default function Hero() {
           />
         ))}
 
-      <div className="pt-32 flex sm:flex-row flex-col max-w-full relative gap-12 lg:gap-40 transform translate-x-[60px] md:translate-x-[100px] lg:translate-x-[60px]">
+      <div className="pt-52 sm:pt-32 flex sm:flex-row flex-col max-w-full relative gap-12 lg:gap-40 transform translate-x-[60px] md:translate-x-[100px] lg:translate-x-[60px]">
         <AnimatePresence>
           {showOtherElements && (
             <motion.div
               initial={{ opacity: 0, x: -50 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.8, ease: "easeOut" }}
-              className="w-[120px] md:w-[160px] lg:w-[210px] lg:translate-x-10"
+              aria-label="logo-systatum-mobile-to-small"
+              className="w-[120px] md:w-[160px] lg:w-[210px] left-16 top-[280px] md:hidden"
             >
               <Image alt="Systatum Logo" src={SystatumLogo} width={300} />
             </motion.div>
@@ -103,8 +120,8 @@ export default function Hero() {
           className={cn(
             "font-light items-center w-full justify-start text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[350px] sm:max-w-[400px] md:max-w-[500px] lg:max-w-[600px]",
             showOtherElements
-              ? ""
-              : "absolute top-[295px] sm:top-[128px] sm:left-[167px] md:left-[208px] lg:left-[370px]"
+              ? "md:translate-x-[200px] lg:translate-x-[320px]"
+              : "absolute top-[376px] sm:top-[128px] sm:left-[167px] md:left-[208px] lg:left-[370px]"
           )}
         >
           {displayedText}
@@ -128,13 +145,13 @@ export default function Hero() {
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, ease: "easeOut", delay: 0.3 }}
-            className="flex flex-row translate-x-[60px] sm:translate-x-[230px] md:translate-x-[310px] lg:translate-x-[430px] sm:mb-0 mb-10 max-w-[330px] md:max-w-[400px] lg:max-w-[600px] relative gap-3"
+            className="flex flex-row translate-x-[60px] sm:translate-x-[230px] md:translate-x-[310px] lg:translate-x-[380px] sm:mb-0 mb-10 max-w-[330px] md:max-w-[400px] lg:max-w-[600px] relative gap-3"
           >
             <span
               aria-label="divider-vertical"
               className="w-[3px] top-[2px] min-h-[70px] sm:min-h-[90px] lg:min-h-[120px] absolute border border-gray-600 bg-gray-600"
             ></span>
-            <span className="ml-4 md:ml-10 sm:max-w-full max-w-[300px] text-lg sm:text-xl md:text-2xl lg:text-4xl">
+            <span className="ml-4 md:ml-10 sm:max-w-full max-w-[300px] text-xl md:text-2xl lg:text-4xl">
               We empower institutions, organizations, and builders with systems
               that matter.
             </span>
