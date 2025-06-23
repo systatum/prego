bf7f22865b28639bab6690f8c387ffd3fbfaf824
Adam Hakarsa <adamnoto@hey.com>
2025-06-23 15:25:42 +0700

    Set for reactStrictMode from isProdBuild

--- apps/systatum/next.config.ts
+++ apps/systatum/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProdBuild = process.env.BUILD_ENV === "production";
 const nextConfig: NextConfig = {
-  output: process.env.BUILD_ENV === "production" ? "export" : "standalone",
+  output: isProdBuild ? "export" : "standalone",
+  reactStrictMode: isProdBuild,
   distDir: "out",
   /* config options here */
 };
