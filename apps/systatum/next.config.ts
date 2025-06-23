2ef9c7a77e12f1347a982c78543e37210478ad56
Adam Hakarsa <adamnoto@hey.com>
2025-06-23 15:16:45 +0700

    Specify the out folder for Systatum

--- apps/systatum/next.config.ts
+++ apps/systatum/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: process.env.BUILD_ENV === "production" ? "export" : "standalone",
+  distDir: "out",
   /* config options here */
 };
 
