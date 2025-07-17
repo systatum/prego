37d6b4b68ffd928228ae742e832be50e823b0b92
Alimnfl <mnaufallim234@gmail.com>
2025-07-17 18:26:02 +0700

    Delete for routes (for build next, cannot detect on middleware)

--- apps/workaty/constants/Routes.ts
+++ apps/workaty/constants/Routes.ts
@@ -1,4 +1,3 @@
 export const PROTECTED_ROUTES = ["/dashboard"];
 export const SESSION_RELATED_ROUTES = ["/signin", "/signup"];
 export const LOCALE_REGEX = /^\/(en|id)(\/|$)/;
-export const MATCHER_ROUTES = ["/admin", "/admin/:path*"];
