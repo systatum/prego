6032e2f715f476957aa559d070db1bc26df0a3b6
Alimnfl <mnaufallim234@gmail.com>
2025-08-13 15:02:53 +0700

    Add react hot toast on layout.

--- apps/systatum/app/layout.tsx
+++ apps/systatum/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { DEFAULT_METADATA } from "@/constants/GetMetaData";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster position="top-right" reverseOrder={false} />
       </body>
     </html>
   );
