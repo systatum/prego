3506e361911cdaeaa2a6bc7c5904cc1e1f4fd90a
Alimnfl <mnaufallim234@gmail.com>
2025-08-20 00:38:55 +0700

    Add page and client detect post functionality.

---
+++ apps/systatum/app/post/page.tsx
@@ -0,0 +1,40 @@
+import Layout from "@/components/layout/layout";
+import client from "@/tina/__generated__/client";
+import PostsClientPage from "./client-page";
+import { POST_METADATA } from "@/constants/GetMetaData";
+
+export const metadata = POST_METADATA;
+export const revalidate = 300;
+
+export default async function PostsPage() {
+  let posts = await client.queries.postConnection({
+    sort: "date",
+    last: 1,
+  });
+  const allPosts = posts;
+
+  if (!allPosts.data.postConnection.edges) {
+    return [];
+  }
+
+  while (posts.data?.postConnection.pageInfo.hasPreviousPage) {
+    posts = await client.queries.postConnection({
+      sort: "date",
+      before: posts.data.postConnection.pageInfo.endCursor,
+    });
+
+    if (!posts.data.postConnection.edges) {
+      break;
+    }
+
+    allPosts.data.postConnection.edges.push(
+      ...posts.data.postConnection.edges.reverse()
+    );
+  }
+
+  return (
+    <Layout rawPageData={allPosts.data}>
+      <PostsClientPage {...allPosts} />
+    </Layout>
+  );
+}
