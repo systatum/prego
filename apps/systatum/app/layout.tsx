d98b42f3e62ff93ec31847d2d993e77d12def8a0
Alimnfl <mnaufallim234@gmail.com>
2025-08-20 15:42:55 +0700

    Add setup locale on layout.

--- apps/systatum/app/layout.tsx
+++ apps/systatum/app/layout.tsx
@@ -2,6 +2,11 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { DEFAULT_METADATA } from "@/constants/GetMetaData";
 import { Toaster } from "react-hot-toast";
+import { TinaUrlFixer } from "@/helper/tina-url-fixer";
+import { NextIntlClientProvider } from "next-intl";
+import { headers } from "next/headers";
+import { LOCALES } from "@/constants/Locale";
+import { VideoDialogProvider } from "../../../packages/components/ui/video-dialog-context";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,18 +20,30 @@ const geistMono = Geist_Mono({
 
 export const metadata = DEFAULT_METADATA;
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const headersList = await headers();
+
+  const locale = headersList.get("X-WORKATY-LOCALE") || LOCALES.EN_US.id;
+
+  const host = headersList.get("host");
+  const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+  const res = await fetch(`${protocol}://${host}/messages/${locale}.json`);
+  const messages = await res.json();
+
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Toaster position="top-right" reverseOrder={false} />
+        <TinaUrlFixer />
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <VideoDialogProvider>{children}</VideoDialogProvider>
+          <Toaster position="top-right" reverseOrder={false} />
+        </NextIntlClientProvider>
       </body>
     </html>
   );
