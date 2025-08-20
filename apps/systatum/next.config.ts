3bcbb77f9db2e257469f709888d4b3a1f1357b78
Alimnfl <mnaufallim234@gmail.com>
2025-08-20 15:45:34 +0700

    Add configuration on next config.

--- apps/systatum/next.config.ts
+++ apps/systatum/next.config.ts
@@ -1,11 +1,51 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
-const isProdBuild = process.env.BUILD_ENV === "production";
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig: NextConfig = {
-  // output: isProdBuild ? "export" : "standalone",
-  // reactStrictMode: isProdBuild,
-  // distDir: "out",
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+      },
+    ];
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.tina.io",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  async headers() {
+    const headers = [
+      {
+        key: "X-Frame-Options",
+        value: "SAMEORIGIN",
+      },
+      {
+        key: "Content-Security-Policy",
+        value: "frame-ancestors 'self'",
+      },
+    ];
+    return [
+      {
+        source: "/((?!admin/).*)",
+        headers,
+      },
+    ];
+  },
+  env: {
+    NEXT_PUBLIC_TINA_MODE: process.env.NEXT_PUBLIC_TINA_MODE,
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
