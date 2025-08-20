4616698674e43631752e508a0351b4ccd9243a33
Alimnfl <mnaufallim234@gmail.com>
2025-08-20 11:01:50 +0700

    Update next js configuration (we can't using static for tina).

--- apps/systatum/next.config.ts
+++ apps/systatum/next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const isProdBuild = process.env.BUILD_ENV === "production";
 const nextConfig: NextConfig = {
-  output: isProdBuild ? "export" : "standalone",
-  reactStrictMode: isProdBuild,
-  distDir: "out",
+  // output: isProdBuild ? "export" : "standalone",
+  // reactStrictMode: isProdBuild,
+  // distDir: "out",
   /* config options here */
 };
 
