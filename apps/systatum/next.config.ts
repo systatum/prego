985135c1a8f8e8df2373681800e9c95380afb1e5
Alimnfl <mnaufallim234@gmail.com>
2025-08-21 14:26:20 +0700

    Update compiler on next js config.

--- apps/systatum/next.config.ts
+++ apps/systatum/next.config.ts
@@ -4,6 +4,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   async rewrites() {
     return [
       {
