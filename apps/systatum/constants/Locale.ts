4080342bd670c81e8c3fce751622e2f1b1f6c0a3
Alimnfl <mnaufallim234@gmail.com>
2025-08-22 15:20:26 +0700

    Add local to mapping languages (for temporary english bug - if not US).

--- apps/systatum/constants/Locale.ts
+++ apps/systatum/constants/Locale.ts
@@ -8,5 +8,19 @@ export interface LocalesDataProps {
 export const LOCALES = {
   EN_US: { id: "en-US", name: "English (US)" },
   ID: { id: "id-ID", name: "Bahasa Indonesia" },
-  JP: { id: "ja-JP", name: "Japanese" },
+  JA: { id: "ja-JP", name: "Japanese" },
 } as const satisfies Record<string, LocalesDataProps>;
+
+export const LOCALE_MAP: Record<string, string> = {
+  en: "en-US",
+  "en-US": "en-US",
+  "en-GB": "en-US",
+  "en-CA": "en-US",
+  "en-AU": "en-US",
+  "en-NZ": "en-US",
+  "en-IN": "en-US",
+  "en-SG": "en-US",
+  "en-PH": "en-US",
+  id: "id-ID",
+  ja: "ja-JP",
+};
