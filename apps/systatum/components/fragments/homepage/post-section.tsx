036eeb6b9db211fe720ee1a3f31bb59c04555001
Alimnfl <mnaufallim234@gmail.com>
2025-08-22 12:51:55 +0700

    Make same method like post page.

--- apps/systatum/components/fragments/homepage/post-section.tsx
+++ apps/systatum/components/fragments/homepage/post-section.tsx
@@ -1,37 +1,5 @@
-import React from "react";
-import client from "@/tina/__generated__/client";
-import Layout from "@/components/layout/layout";
-import PostsClientPage from "./../../../app/post/client-page";
+import PostsPage from "@/app/post/page";
 
 export default async function PostSection() {
-  let posts = await client.queries.postConnection({
-    sort: "date",
-    last: 1,
-  });
-  const allPosts = posts;
-
-  if (!allPosts.data.postConnection.edges) {
-    return [];
-  }
-
-  while (posts.data?.postConnection.pageInfo.hasPreviousPage) {
-    posts = await client.queries.postConnection({
-      sort: "date",
-      before: posts.data.postConnection.pageInfo.endCursor,
-    });
-
-    if (!posts.data.postConnection.edges) {
-      break;
-    }
-
-    allPosts.data.postConnection.edges.push(
-      ...posts.data.postConnection.edges.reverse()
-    );
-  }
-
-  return (
-    <Layout rawPageData={allPosts.data}>
-      <PostsClientPage {...allPosts} />
-    </Layout>
-  );
+  return <PostsPage />;
 }
