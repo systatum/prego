3a42d9a2fdbc0a1086acd331e073f64456267e2a
Alimnfl <mnaufallim234@gmail.com>
2025-08-22 12:50:05 +0700

    Add locales on constant.

--- apps/systatum/constants/Locale.ts
+++ apps/systatum/constants/Locale.ts
@@ -1,4 +1,4 @@
-export type LocaleCodeProps = "en-US" | "id";
+export type LocaleCodeProps = "en-US" | "id-ID" | "ja-JP";
 
 export interface LocalesDataProps {
   id: LocaleCodeProps;
@@ -7,5 +7,6 @@ export interface LocalesDataProps {
 
 export const LOCALES = {
   EN_US: { id: "en-US", name: "English (US)" },
-  ID: { id: "id", name: "Bahasa Indonesia" },
+  ID: { id: "id-ID", name: "Bahasa Indonesia" },
+  JP: { id: "ja-JP", name: "Japanese" },
 } as const satisfies Record<string, LocalesDataProps>;
