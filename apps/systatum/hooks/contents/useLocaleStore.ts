3e14b422208572b1d93206a8fab31c6c29de4b41
Alimnfl <mnaufallim234@gmail.com>
2025-08-23 16:30:58 +0700

    Update store data using code, label and flag.

--- apps/systatum/hooks/contents/useLocaleStore.ts
+++ apps/systatum/hooks/contents/useLocaleStore.ts
@@ -1,14 +1,40 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
-import { useLocaleStoreProps } from "../types/useLocaleStore";
-import { LocaleCodeProps } from "@/constants/Locale";
+import {
+  OptionCountryProps,
+  useLocaleStoreProps,
+} from "../types/useLocaleStore";
+import { LOCALE_MAP } from "@/constants/Locale";
+
+function getInitialLocale(): OptionCountryProps {
+  if (typeof document !== "undefined") {
+    const match = document.cookie.match(/SYSTATUM_LOCALE=([^;]+)/);
+    if (match) {
+      const rawLocale = match[1];
+      const code = LOCALE_MAP[rawLocale] ?? "en-US";
+
+      return {
+        code,
+        label:
+          code === "en-US"
+            ? "United States"
+            : code === "id-ID"
+              ? "Indonesia"
+              : "Japan",
+        flag: code === "en-US" ? "🇺🇸" : code === "id-ID" ? "🇮🇩" : "🇯🇵",
+      };
+    }
+  }
+
+  return { code: "en-US", label: "United States", flag: "🇺🇸" };
+}
 
 export const useLocaleStore = create<useLocaleStoreProps>()(
   persist(
     (set) => ({
-      locale: "en-US",
-      setLocale: (locale: LocaleCodeProps) => {
-        document.cookie = `SYSTATUM_LOCALE=${locale}; path=/`;
+      locale: getInitialLocale(),
+      setLocale: (locale: OptionCountryProps) => {
+        document.cookie = `SYSTATUM_LOCALE=${locale.code}; path=/`;
         set({ locale });
         window.location.reload();
       },
@@ -19,3 +45,9 @@ export const useLocaleStore = create<useLocaleStoreProps>()(
     }
   )
 );
+
+export const OPTIONS_COUNTRY: OptionCountryProps[] = [
+  { code: "en-US", label: "English", flag: "🇺🇸" },
+  { code: "id-ID", label: "Bahasa Indonesia", flag: "🇮🇩" },
+  { code: "ja-JP", label: "日本語", flag: "🇯🇵" },
+];
