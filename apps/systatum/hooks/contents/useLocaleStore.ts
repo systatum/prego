7529df1ea803cdd422b6b00b79bd968d091a9548
Alimnfl <mnaufallim234@gmail.com>
2025-08-24 14:13:06 +0700

    Update for locale store data.

--- apps/systatum/hooks/contents/useLocaleStore.ts
+++ apps/systatum/hooks/contents/useLocaleStore.ts
@@ -4,35 +4,11 @@ import {
   OptionCountryProps,
   useLocaleStoreProps,
 } from "../types/useLocaleStore";
-import { LOCALE_MAP } from "@/constants/Locale";
-
-function getInitialLocale(): OptionCountryProps {
-  if (typeof document !== "undefined") {
-    const match = document.cookie.match(/SYSTATUM_LOCALE=([^;]+)/);
-    if (match) {
-      const rawLocale = match[1];
-      const code = LOCALE_MAP[rawLocale] ?? "en-US";
-
-      return {
-        code,
-        label:
-          code === "en-US"
-            ? "United States"
-            : code === "id-ID"
-              ? "Indonesia"
-              : "Japan",
-        flag: code === "en-US" ? "🇺🇸" : code === "id-ID" ? "🇮🇩" : "🇯🇵",
-      };
-    }
-  }
-
-  return { code: "en-US", label: "United States", flag: "🇺🇸" };
-}
 
 export const useLocaleStore = create<useLocaleStoreProps>()(
   persist(
     (set) => ({
-      locale: getInitialLocale(),
+      locale: { code: "en-US", label: "English", flag: "🇺🇸" },
       setLocale: (locale: OptionCountryProps) => {
         document.cookie = `SYSTATUM_LOCALE=${locale.code}; path=/`;
         set({ locale });
