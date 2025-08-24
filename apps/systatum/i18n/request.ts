634b24f55ded2fc298c3c9c3aac4c2b8b531bf5d
Alimnfl <mnaufallim234@gmail.com>
2025-08-24 14:11:37 +0700

    Update request on i18n method (config).

--- apps/systatum/i18n/request.ts
+++ apps/systatum/i18n/request.ts
@@ -1,19 +1,20 @@
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 import { cookies, headers } from "next/headers";
-import { LOCALE_MAP, LocaleCodeProps } from "@/constants/Locale";
+import { LOCALE_MAP, LocaleCodeProps, LOCALES } from "@/constants/Locale";
 
 export default getRequestConfig(async () => {
-  const cookieStore = cookies();
-  let rawLocale = (await cookieStore).get("SYSTATUM_LOCALE")?.value;
+  const headersList = await headers();
+  const cookieStore = await cookies();
+
+  const acceptLang = headersList.get("Accept-Language").split(",")[0];
+  let rawLocale = cookieStore.get("SYSTATUM_LOCALE")?.value || acceptLang;
 
   if (!rawLocale || !routing.locales.includes(rawLocale as LocaleCodeProps)) {
     rawLocale = routing.defaultLocale;
   }
 
-  let locale = LOCALE_MAP[rawLocale] ?? "en-US";
-
-  const headersList = await headers();
+  let locale = LOCALE_MAP[rawLocale] ?? LOCALES.EN_US.id;
 
   const host = headersList.get("host");
   const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
