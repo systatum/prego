db36b135c153052c062159190d0368c5030d0ee8
Alimnfl <mnaufallim234@gmail.com>
2025-08-26 17:49:58 +0700

    Update meta data to export.

--- apps/systatum/constants/GetMetaData.ts
+++ apps/systatum/constants/GetMetaData.ts
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
-const BASE_TITLE = "Systatum";
-const BASE_DESCRIPTION =
+export const BASE_TITLE = "Systatum";
+export const BASE_DESCRIPTION =
   "Systatum empowers institutions, organizations, and builders with systems that matter.";
 
 export function createMetadata(title?: string, description?: string): Metadata {
