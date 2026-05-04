bea60e1b95edb7e23c6a7485a1a2e48797979abc
Alimnfl <mnaufallim234@gmail.com>
2026-05-04 14:44:49 +0700

    Add url admin fixer.

--- apps/sequelore/app/layout.tsx
+++ apps/sequelore/app/layout.tsx
@@ -4,9 +4,9 @@ import { Inter as FontSans, Lato, Nunito } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { VideoDialogProvider } from "@/components/ui/VideoDialogContext";
 import VideoDialog from "@/components/ui/VideoDialog";
-
 import "@/styles.css";
 import { TailwindIndicator } from "@/components/ui/breakpoint-indicator";
+import { TinaUrlFixer } from "@/helpers/tina-url-fixer";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -35,8 +35,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cn(fontSans.variable, nunito.variable, lato.variable)}>
+    <html
+      lang="en"
+      className={cn(fontSans.variable, nunito.variable, lato.variable)}
+    >
       <body className="min-h-screen bg-background font-sans antialiased">
+        <TinaUrlFixer />
         <VideoDialogProvider>
           {children}
           <VideoDialog />
