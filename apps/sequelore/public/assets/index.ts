83fed5b25420ce513d2f77698f453c930e4025fa
Alimnfl <mnaufallim234@gmail.com>
2026-05-05 16:01:41 +0700

    Add systatum image.

--- apps/sequelore/public/assets/index.ts
+++ apps/sequelore/public/assets/index.ts
@@ -1,5 +1,6 @@
 import HeroBackground from "./hero-background.png";
 import WaterImage from "./water.png";
 import DuckSwimming from "./duck.png";
+import SystatumImage from "./systatum_256.png";
 
-export { HeroBackground, DuckSwimming, WaterImage };
+export { HeroBackground, DuckSwimming, WaterImage, SystatumImage };
