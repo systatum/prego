4725b974f8393201c6a0b6f5e7bb5b31f01b69d3
Alimnfl <mnaufallim234@gmail.com>
2026-05-05 10:39:51 +0700

    Update container on the landing page through 1 component.

--- apps/sequelore/components/layout/container.tsx
+++ apps/sequelore/components/layout/container.tsx
@@ -1,19 +1,24 @@
 "use client";
 
 import { ReactNode } from "react";
-import styled, { CSSProp } from "styled-components";
+import styled, { css, CSSProp } from "styled-components";
 
-export function Container({
+interface InnerProps {
+  $reverse?: boolean;
+  $style?: CSSProp;
+}
+
+function Container({
   styles,
   children,
 }: {
   styles?: { self?: CSSProp };
   children?: ReactNode;
 }) {
-  return <Wrapper $style={styles?.self}>{children}</Wrapper>;
+  return <BaseContainer $style={styles?.self}>{children}</BaseContainer>;
 }
 
-const Wrapper = styled.div<{ $style?: CSSProp }>`
+const BaseContainer = styled.div<{ $style?: CSSProp }>`
   display: flex;
   flex-direction: column;
   gap: 20px;
@@ -24,3 +29,91 @@ const Wrapper = styled.div<{ $style?: CSSProp }>`
 
   ${({ $style }) => $style}
 `;
+
+function Section({
+  styles,
+  children,
+  reverse = false,
+}: {
+  styles?: { self?: CSSProp };
+  children?: ReactNode;
+  reverse?: boolean;
+}) {
+  return (
+    <BaseSection $reverse={reverse} $style={styles?.self}>
+      {children}
+    </BaseSection>
+  );
+}
+
+const BaseSection = styled.section<InnerProps>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 4rem;
+  position: relative;
+
+  ${({ $reverse }) =>
+    $reverse &&
+    css`
+      backdrop-filter: brightness(0.96) saturate(0.9);
+    `}
+
+  @media (max-width: 768px) {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  @media (max-width: 640px) {
+    padding: 80px 30px;
+    flex-direction: column;
+  }
+
+  ${({ $style }) => $style}
+`;
+
+function Inner({
+  styles,
+  children,
+  reverse = false,
+}: {
+  styles?: { self?: CSSProp };
+  children?: ReactNode;
+  reverse?: boolean;
+}) {
+  return (
+    <BaseInner $reverse={reverse} $style={styles?.self}>
+      {children}
+    </BaseInner>
+  );
+}
+
+const BaseInner = styled.div<InnerProps>`
+  margin: 0 auto;
+  gap: 5rem;
+  align-items: center;
+  max-width: 1280px;
+  display: flex;
+  flex-direction: row;
+
+  ${({ $reverse }) =>
+    $reverse &&
+    css`
+      direction: rtl;
+      > * {
+        direction: ltr;
+      }
+    `}
+
+  @media (max-width: 640px) {
+    flex-direction: column;
+  }
+
+  ${({ $style }) => $style}
+`;
+
+Container.Section = Section;
+Section.Inner = Inner;
+
+export { Container };
