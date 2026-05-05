3bc861a59cd6152abf5431d45be68b2e41b16bab
Alimnfl <mnaufallim234@gmail.com>
2026-05-05 16:16:22 +0700

    Remove unnecessary content.

--- apps/sequelore/app/page.tsx
+++ apps/sequelore/app/page.tsx
@@ -1,5 +1,4 @@
 import { Container } from "@/components/layout/container";
-import { Navbar } from "@/fragments/root/navbar";
 import { Features } from "@/fragments/root/features";
 import { Footer } from "@/fragments/root/footer";
 import { Hero } from "@/fragments/root/hero";
@@ -9,7 +8,6 @@ import { OurCompany } from "@/fragments/root/our-company";
 export default function Home() {
   return (
     <Container>
-      <Navbar />
       <Hero />
       <Features />
       <TrustedBy />
