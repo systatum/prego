4b8c37ca89a7eef20df031d1e9b5ad5049d37a22
Alimnfl <mnaufallim234@gmail.com>
2026-05-05 15:24:43 +0700

    Remove all tina config in layout.

--- apps/sequelore/app/layout.tsx
+++ apps/sequelore/app/layout.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import { Metadata } from "next";
 import { Inter as FontSans, Lato, Nunito } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { VideoDialogProvider } from "@/components/ui/VideoDialogContext";
-import VideoDialog from "@/components/ui/VideoDialog";
 import "@/styles.css";
-import { TinaUrlFixer } from "@/helpers/tina-url-fixer";
 import StyledComponentRegistry from "@/lib/styled-component-registry";
 
 const fontSans = FontSans({
@@ -41,13 +38,7 @@ export default function RootLayout({
       className={cn(fontSans.variable, nunito.variable, lato.variable)}
     >
       <body className="min-h-screen bg-background font-sans antialiased">
-        <StyledComponentRegistry>
-          <TinaUrlFixer />
-          <VideoDialogProvider>
-            {children}
-            <VideoDialog />
-          </VideoDialogProvider>
-        </StyledComponentRegistry>
+        <StyledComponentRegistry>{children}</StyledComponentRegistry>
       </body>
     </html>
   );
