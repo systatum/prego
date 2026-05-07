1c43a599708460a5a95b4250faac0e5f56aee8df
Alimnfl <mnaufallim234@gmail.com>
2026-05-07 18:25:31 +0700

    Add tina config.

---
+++ apps/systatum-1/tina/config.tsx
@@ -0,0 +1,33 @@
+import { defineConfig } from "tinacms";
+import Post from "./collection/post";
+import Global from "./collection/global";
+import Author from "./collection/author";
+import Page from "./collection/page";
+import Tag from "./collection/tag";
+import Category from "./collection/category";
+
+const config = defineConfig({
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
+  branch:
+    process.env.NEXT_PUBLIC_TINA_BRANCH! ||
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! ||
+    process.env.HEAD! ||
+    "main",
+  token: process.env.TINA_TOKEN!,
+  media: {
+    tina: {
+      publicFolder: "static",
+      mediaRoot: "uploads",
+    },
+  },
+  build: {
+    publicFolder: "static",
+    outputFolder: "admin",
+    basePath: "",
+  },
+  schema: {
+    collections: [Page, Post, Category, Author, Tag, Global],
+  },
+});
+
+export default config;
