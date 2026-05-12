a5f11cb878191f7dc2d3d0f7760a3d29d24df6c3
Alimnfl <mnaufallim234@gmail.com>
2026-05-12 10:27:59 +0700

    Update the config for browser and ssr.

--- apps/systatum/gatsby-config.ts
+++ apps/systatum/gatsby-config.ts
@@ -6,24 +6,7 @@ const config: GatsbyConfig = {
     siteUrl: `https://systatum.com`,
   },
   graphqlTypegen: true,
-  plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-styled-components",
-    {
-      resolve: "gatsby-plugin-react-i18next",
-      options: {
-        localeJsonSourceName: "locale",
-        languages: ["en-US", "id-ID", "ja-JP"],
-        defaultLanguage: "en-US",
-        siteUrl: "https://systatum.com",
-        ns: ["common", "postPage"],
-        defaultNS: "common",
-        i18nextOptions: {
-          interpolation: { escapeValue: false },
-        },
-      },
-    },
-  ],
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-styled-components"],
 };
 
 export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
